59fbf9d1a49e7c0ea2eaec47852dcef98ac711e0
Raffael Zica <raffael@pixeltastic.de>
2018-11-07 05:32:56 +0100

    Introducing: sush, run your shell command with pkexec!

--- src/macros.rs
+++ src/macros.rs
@@ -39,12 +39,38 @@ macro_rules! sh_command {
 }
 
 #[macro_export]
+macro_rules! sush_command {
+    ($fmt:expr) => ( sush_command!($fmt,) );
+    ($fmt:expr, $( $id:ident = $value:expr ),* $(,)*) => (
+        $crate::commandify(
+            format!(
+                "pkexec sh -c {}",
+                $crate::command_arg(
+                    &format!("set -e\n\n{}", format!($fmt, $( $id = $crate::command_arg(&$value) ,)*)),
+                ),
+            )
+        )
+    );
+}
+
+#[macro_export]
 macro_rules! sh_execute {
     ($fmt:expr) => ( sh_execute!($fmt,) );
     ($fmt:expr, $( $id:ident = $value:expr ),* $(,)*) => (
         {
             use $crate::{CommandSpecExt};
             sh_command!($fmt, $( $id = $value ),*).unwrap().execute()
+        }
+    );
+}
+
+#[macro_export]
+macro_rules! sush_execute {
+    ($fmt:expr) => ( sush_execute!($fmt,) );
+    ($fmt:expr, $( $id:ident = $value:expr ),* $(,)*) => (
+        {
+            use $crate::{CommandSpecExt};
+            sush_command!($fmt, $( $id = $value ),*).unwrap().execute()
         }
     );
 }
